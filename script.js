b1758f8eed4da5c1af5834943d9a72fbfcd35cfa
BrenoAndrade03 <brenopereira263@gmail.com>
2022-02-01 21:13:09 -0300

    while-for versão 1.0

---
+++ script.js
@@ -0,0 +1,25 @@
+//exercício1
+for(let i=1; i<=10;i++){
+    console.log(i);
+}
+console.log("Fim do primeiro exercício.");
+//exercício2
+let num =10;
+while(num>=1){
+    console.log(num);
+    num--;
+}
+console.log("Fim do segundo exercício.");
+//exercicio3
+for(i=1; i<=100; i+=2){
+    console.log(i);
+}
+console.log("Fim do 3° exercício. Esse foi cansativo");
+//exercicio4
+        num=0;
+while(num<=100){
+    console.log(num);
+    num+=2;
+}
+console.log("Fim do 4° exercício.");
+console.log("Valeu por observar até aqui♥!");
